358552aa33300e10fc1d0a3812d2f5810aad9c41
llunasanz <lluna@declarando.es>
2022-01-21 18:11:34 +0100

    [2022/01/21 18:10] Code for generate dataframes from users and their distances

--- datagenerator.py
+++ datagenerator.py
@@ -1,8 +1,12 @@
 # NO TOCAR
+from math import sin, cos, sqrt, atan2, radians
 from faker import Faker
 import keyboard
+import json
+import pandas as pd
 import time
 import random
+from datetime import datetime
 
 faker = Faker('es_ES')
 USERS_TOTAL=100
@@ -56,9 +60,21 @@ def generate_step():
         initiate_data()
     return users
 
+def get_distance_km(lat1, lat2, lon1, lon2):
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = R * c
+    return distance
 
 
 # NO TOCAR
+# Users dataframe initialization
+# FIXME: find the way to avoid memory management problems. Maybe, removing the first USERS_TOTAL rows when the number of iterations is 10 or 5.
+df = pd.DataFrame()
+# Users dataframe with kms and score
+df_real_time = pd.DataFrame(columns = ['id', 'km_walk', 'km_bike', 'score'])
 
 while True:
     try:  
@@ -74,8 +90,31 @@ while True:
             time.sleep(2)
             temp = pd.read_json('users_test.json')
             temp = temp.T
+            temp['lat'] = list(map(lambda x: x.get('lat'), list(temp.position)))
+            temp['lon'] = list(map(lambda x: x.get('lon'), list(temp.position)))
+            temp.drop(columns='position')
             df = df.append(temp)
-            time.sleep(2)
+            
+            if len(df.loc[df.id == df.id.iloc[0]])>1:
+                # Add values
+                df_real_time['km_walk'] = list(map(lambda x: get_distance_km(df.loc[df.id == x].lat[-2], df.loc[df.id == x].lat[-1], df.loc[df.id == x].lon[-2], df.loc[df.id == x].lon[-1]) 
+                                                   if df.loc[df.id == x].transport.iloc[-1] == 'Walking' and df.loc[df.id == x].transport.iloc[-2] == 'Walking'
+                                                   else 0, 
+                                                   list(df_real_time.id.unique()))) + df_real_time['km_walk']
+                df_real_time['km_bike'] = list(map(lambda x: get_distance_km(df.loc[df.id == x].lat[-2], df.loc[df.id == x].lat[-1], df.loc[df.id == x].lon[-2], df.loc[df.id == x].lon[-1]) 
+                                                   if df.loc[df.id == x].transport.iloc[-1] == 'Bike' and df.loc[df.id == x].transport.iloc[-2] == 'Bike'
+                                                   else 0, 
+                                                   list(df_real_time.id.unique()))) + df_real_time['km_bike']
+            else:
+                # Initialize dataframe
+                df_real_time['id'] = list(df.id.unique())
+                df_real_time['km_walk'] = 0
+                df_real_time['km_bike'] = 0
+                df_real_time['km_score'] = 0
+            
+            df.to_csv('users_stats.csv')
+            df_real_time.to_csv('users_real_time.csv')
+
     except Exception as err:
         print(f"Unexpected {err}, {type(err)}")
-        break  
+        break
