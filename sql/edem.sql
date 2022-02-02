59b55bcdfce183a1f5d0a61b42ca5f29b52a4acd
Alex <alpeca2@edem.es>
2022-02-02 18:08:22 +0100

    Cambio dashboard

--- sql/edem.sql
+++ sql/edem.sql
@@ -1,8 +1,9 @@
 create database zurich;
+use zurich;
 
 create table Custom
 (
-    Id_custom varchar(20) primary key not null,
+    Id_custom varchar(20) primary key,
     customName varchar(100) not null,
     customLastName varchar(100) null,
     age int not null,
@@ -16,8 +17,7 @@ create table Custom
     drinking float null,
     disability bit null,
     previousPathology bit  null,
-    CP int not null
-    
+    CP int not null  
 );
 
 create table position
@@ -28,19 +28,26 @@ create table position
     latitude float not null,
     longitude float not null,
     time datetime not null,
-        foreign key (Id_custom) references Custom (id_custom) ON DELETE CASCADE);
+        foreign key (Id_custom) references Custom (id_custom) ON DELETE CASCADE
+);
   
 create table friends
 (
-    Id_friends int auto_increment primary key,
     Id_custom varchar(20) not null,
-    friend varchar(20) not null,
-        foreign key (Id_custom) references Custom(Id_custom) ON DELETE CASCADE,
-        foreign key (friend) references Custom(Id_custom) ON DELETE CASCADE
+    friend_0 varchar(20) null,
+    friend_1 varchar(20) null,
+    friend_2 varchar(20) null,
+    friend_3 varchar(20) null,
+    friend_4 varchar(20) null,
+    friend_5 varchar(20) null,
+    friend_6 varchar(20) null,
+    friend_7 varchar(20) null,
+    friend_8 varchar(20) null,
+    friend_9 varchar(20) null,
+        foreign key (Id_custom) references Custom(Id_custom) ON DELETE CASCADE
 );
 
-create table ranking(
-
+create table ranking (
     Id_custom varchar(20) primary key,
     Km_walking float null,
     km_bike float null,
